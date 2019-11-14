00765b5c67bb74079181ca18b45223f1a71d1b55
abanoubmesiha <bony.nagy@gmail.com>
2019-11-14 21:38:31 +0200

    3 my answer

---
+++ 3-first.js
@@ -0,0 +1,16 @@
+function first(arr,n=1) {
+    if (n < 1 ) { n = 0 }
+    return arr.slice(0,n);
+}
+
+console.log(first([7, 9, 0, -2]));
+console.log(first([],3));
+console.log(first([7, 9, 0, -2],3));
+console.log(first([7, 9, 0, -2],6));
+console.log(first([7, 9, 0, -2],-3));
+/*
+7
+[]
+[7, 9, 0]
+[7, 9, 0, -2]
+[]*/
