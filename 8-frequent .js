29c1ad31ac72c1efab7a4524bd8af21da33ae838
abanoubmesiha <bony.nagy@gmail.com>
2019-11-16 17:13:43 +0200

    8 my second answer

--- 8-frequent .js
+++ 8-frequent .js
@@ -1,42 +1,32 @@
 //8.Write a JavaScript program to find the most frequent item of an array.
-var arr1=[2,3, 'a', 'a','a','a', 'a',2, 2,2,2,2, 3, 'a', 3, 'a', 2, 4, 9, 3,'b'];
-function frequent(arr){
-    var sorted = arr.sort();
-    var freq = [];
-    var count=1
-    for (let i=1; i<sorted.length;i++){
-        if (sorted[i-1] == sorted[i]){
-            count += 1
-            freq.push(`${sorted[i-1]} (${count}) times`)
+var arr1=[9,4,3,2,'b', 'a', 'a','a','a', 'a',2, 2,2,2,2, 3, 'a', 3, 'a', 2,   3,'b'];
+function mostFreq(arr)    {
+    var count = 0;
+    var checker=2;
+    var counted = [];
+    var result;
+    for (let i=0;i<arr.length;i++){
+        if (counted.includes(arr[i])){continue}
+        for (let x=i;x<arr.length;x++){
+            if (arr[x] == arr[i] ){
+                if(!(counted.includes(arr[x]))){
+                    counted.push(arr[x]);
+                }
+                count++;
+            }
+            else continue;
         }
-        else {
-            count = 1
-            continue;
-        };
+        console.log(arr[i],count,checker)
+        if (count > checker){
+            checker=count
+            result=(`item : ${arr[i]} is found (${count}) times`);
+            count=0;
+        }
+        count = 0;
     }
-    let mapped = freq.map(item => {
-        return Number(item.match(/\d(?!.*\d)/g));
-    })
-    console.log(mapped);
-    let biggest = Math.max(...mapped);
-    console.log(biggest);
-    function multibleBig(arrr,val){
-        var indexes = [];
-        for (let i=0;i<arrr.length;i++){
-           if (arrr[i] == val){indexes.push(i)}
-        }
-        console.log(indexes);
-        console.log(freq[indexes[1]]);
-        var result = [];
-        for (var x=0;x<indexes.length;x++){
-            result.push(freq[indexes[x]]);
-        }
-        console.log(result);
-        return result
-    }
-    return (multibleBig(mapped,biggest)).join(' and ');
+    return result
 }
-console.log(frequent(arr1));
+console.log(mostFreq(arr1));
 /*
 Sample array : var arr1=[2,3, 'a', 'a','a','a', 'a',2, 2,2,2,2, 3, 'a', 3, 'a', 2, 4, 9, 3,'b'];
 Sample Output : 2 (7) times and a (7) times
