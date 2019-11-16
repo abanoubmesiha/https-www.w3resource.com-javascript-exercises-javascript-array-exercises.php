fd3a3706bc93c6994579ea1e1f39afb2660fc43b
abanoubmesiha <bony.nagy@gmail.com>
2019-11-16 18:04:25 +0200

    8 stack overflow shortest answer

--- 8-frequent .js
+++ 8-frequent .js
@@ -1,32 +1,13 @@
 //8.Write a JavaScript program to find the most frequent item of an array.
 var arr1=[9,4,3,2,'b', 'a', 'a','a','a', 'a',2, 2,2,2,2, 3, 'a', 3, 'a', 2,   3,'b'];
-function mostFreq(arr)    {
-    var count = 0;
-    var checker=2;
-    var counted = [];
-    var result;
-    for (let i=0;i<arr.length;i++){
-        if (counted.includes(arr[i])){continue}
-        for (let x=i;x<arr.length;x++){
-            if (arr[x] == arr[i] ){
-                if(!(counted.includes(arr[x]))){
-                    counted.push(arr[x]);
-                }
-                count++;
-            }
-            else continue;
-        }
-        console.log(arr[i],count,checker)
-        if (count > checker){
-            checker=count
-            result=(`item : ${arr[i]} is found (${count}) times`);
-            count=0;
-        }
-        count = 0;
-    }
-    return result
+function mode(arr){
+    let item = arr.sort((a,b) =>
+          arr.filter(v => v===a).length - arr.filter(v => v===b).length
+    ).pop();
+    return `${item} + (${arr.filter(v => v===item).length+1}) times`
 }
-console.log(mostFreq(arr1));
+
+console.log(mode(arr1));
 /*
 Sample array : var arr1=[2,3, 'a', 'a','a','a', 'a',2, 2,2,2,2, 3, 'a', 3, 'a', 2, 4, 9, 3,'b'];
 Sample Output : 2 (7) times and a (7) times
