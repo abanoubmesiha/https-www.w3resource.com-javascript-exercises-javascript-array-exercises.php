80d1cf3d526bbbcfa2a324a0dd5dbccca528cf21
abanoubmesiha <bony.nagy@gmail.com>
2019-11-20 21:55:20 +0200

    10 my answer

---
+++ 10-printNesting.js
@@ -0,0 +1,25 @@
+//10. Write a JavaScript program which prints the elements of the following array.
+var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7],[5,7]];
+console.log(a[0].length)
+function printNesting(arr){
+    var result = []
+        for (let i=0;i<arr.length;i++){
+            result.push(`row ${i}`)
+            for (let x=0;(x<a[i].length);x++){
+                result.push(`${arr[i][x]}`)
+            }
+        }
+    return result.join('\n');
+}
+console.log(printNesting(a));
+/*
+Note : Use nested for loops.
+Sample array : var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+Sample Output :
+"row 0"
+" 1"
+" 2"
+" 1"
+" 24"
+"row 1"
+*/
