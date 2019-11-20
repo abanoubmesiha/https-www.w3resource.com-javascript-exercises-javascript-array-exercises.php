9950dec7322ac837dfbef0cbf8825b88bc71df70
abanoubmesiha <bony.nagy@gmail.com>
2019-11-20 21:56:56 +0200

    10 nice edit from website

--- 10-printNesting.js
+++ 10-printNesting.js
@@ -3,9 +3,9 @@ var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26
 console.log(a[0].length)
 function printNesting(arr){
     var result = []
-        for (let i=0;i<arr.length;i++){
+        for (let i in arr){
             result.push(`row ${i}`)
-            for (let x=0;(x<a[i].length);x++){
+            for (let x in arr[i]){
                 result.push(`${arr[i][x]}`)
             }
         }
